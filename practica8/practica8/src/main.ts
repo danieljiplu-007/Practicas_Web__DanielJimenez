7bfc22f8a18284d06ac06a98a46898edf5865456
danieljiplu-007 <danieljiplu@micorreo.upp.edu.mx>
2026-02-18 20:55:10 -0600

    Create main.ts

---
+++ practica8/practica8/src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    ...appConfig.providers
+  ]
+});
+
