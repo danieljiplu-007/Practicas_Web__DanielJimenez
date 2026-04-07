bde30455231165ece64aff0f9888afca34ed353f
danieljiplu-007 <danieljiplu@micorreo.upp.edu.mx>
2026-04-07 16:02:51 -0600

    Update app.config.ts

--- el-chinaloense-oficial/src/app/app.config.ts
+++ el-chinaloense-oficial/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; 
+
 import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(), 
     importProvidersFrom(FormsModule)
   ]
 };
