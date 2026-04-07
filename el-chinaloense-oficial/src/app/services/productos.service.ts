288a056262bf509b57308322ef2e63eae8feec51
danieljiplu-007 <danieljiplu@micorreo.upp.edu.mx>
2026-04-07 14:30:56 -0600

    Update productos.service.ts

--- el-chinaloense-oficial/src/app/services/productos.service.ts
+++ el-chinaloense-oficial/src/app/services/productos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { Observable, map } from 'rxjs'
 import { Producto } from '../models/producto'
 
 @Injectable({
@@ -8,12 +8,23 @@ import { Producto } from '../models/producto'
 })
 export class ProductosService {
 
-  private apiUrl = 'https://elchinaloense-backend.onrender.com'
+  private apiUrl = 'https://elchinaloense-backend.onrender.com/platillos'
 
   constructor(private http: HttpClient) {}
 
   getProductos(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.apiUrl)
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map(data => data.map(p => ({
+        ...p,
+        ingredientes: p.ingredientes || [],
+        extras: p.extras || [],
+        imagen: p.imagen || '',
+        nombre: p.nombre || '',
+        descripcion: p.descripcion || '',
+        categoria: p.categoria || '',
+        precio: p.precio || 0
+      })))
+    )
   }
 
   agregarProducto(producto: Producto): Observable<any> {
